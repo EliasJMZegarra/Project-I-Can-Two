419f2a1beaf25692c8e939d800a008e279dfb586
EliasJMZegarra <105391465+EliasJMZegarra@users.noreply.github.com>
2024-04-22 15:45:18 -0500

    Delete de la entidad Clínica
    Se decidió eliminar la entidad Clínica debido a que complicaría más el proyecto.

--- src/main/java/upc/com/pe/trabajo_v1/repository/ClinicaRepository.java
+++
@@ -1,7 +0,0 @@
-package upc.com.pe.trabajo_v1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import upc.com.pe.trabajo_v1.entities.Clinica;
-
-public interface ClinicaRepository extends JpaRepository<Clinica, Integer> {
-}
